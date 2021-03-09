efe0bb5656438f754fff3d9324e8cc27ad80a0ae
Vincent Voyer <vincent@codeagain.com>
2021-03-09 11:10:02 +0100

    test

--- pages/[login]/github.js
+++ pages/[login]/github.js
@@ -4,7 +4,18 @@ export default function GitHubPage() {
   const router = useRouter();
   const { login } = router.query;
 
-  console.log(login);
+  console.log(login, router.isFallback);
 
   return <div>Coucou</div>;
+}
+
+export async function getStaticProps(context) {
+  return {props: {}}
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { login: "vvo" } }],
+    fallback: true,
+  };
 }
